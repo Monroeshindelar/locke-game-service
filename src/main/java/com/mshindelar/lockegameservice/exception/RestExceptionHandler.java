d6cce36e039f0828ba06642ab51d0fb18a62e2f2
Monroe Shindelar <monroeshindelar@gmail.com>
2021-07-31 17:43:21 -0700

    LW-8 Added custom exception handling logic to make api errors less ambiguous.

---
+++ src/main/java/com/mshindelar/lockegameservice/exception/RestExceptionHandler.java
@@ -0,0 +1,37 @@
+package com.mshindelar.lockegameservice.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+
+@ControllerAdvice
+public class RestExceptionHandler{
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<ApiErrorResponse> handleHttpMessageNotReadableException(HttpMessageNotReadableException ex) {
+        return buildApiErrorResponse(ex.getMessage(), ex.toString(), HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(value = Exception.class)
+    public ResponseEntity<ApiErrorResponse> handleException(Exception ex) {
+        return buildApiErrorResponse(ex.getMessage(), ex.toString(), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    private ResponseEntity<ApiErrorResponse> buildApiErrorResponse(String message, String exception, HttpStatus httpStatus) {
+        ApiErrorResponse apiResponse = new ApiErrorResponse
+                .ApiErrorResponseBuilder()
+                .withException(exception)
+                .withMessage(message)
+                .withStatus("" + httpStatus.value())
+                .withError(httpStatus)
+                .atTime(LocalDateTime.now(ZoneOffset.UTC))
+                .build();
+
+        return new ResponseEntity<>(apiResponse, httpStatus);
+    }
+}
