d21488f9a680b313632a87b9986ab25560e6debb
TannerDryden <tdd7197@gmail.com>
2021-08-19 19:22:24 -0700

    Added get all game generation ids endpoint

--- src/main/java/com/mshindelar/lockegameservice/repository/GameGenerationRepository.java
+++ src/main/java/com/mshindelar/lockegameservice/repository/GameGenerationRepository.java
@@ -2,8 +2,10 @@ package com.mshindelar.lockegameservice.repository;
 
 import com.mshindelar.lockegameservice.entity.generic.GameGeneration;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
---
+++ src/main/java/com/mshindelar/lockegameservice/service/GameGenerationService.java
@@ -0,0 +1,25 @@
+package com.mshindelar.lockegameservice.service;
+import com.mshindelar.lockegameservice.controller.GameGenerationController;
+import com.mshindelar.lockegameservice.entity.generic.GameGeneration;
+import com.mshindelar.lockegameservice.repository.GameGenerationRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@Slf4j
+public class GameGenerationService {
+
+    @Autowired
+    private GameGenerationRepository gameGenerationRepository;
+
+    private static Logger logger = LoggerFactory.getLogger(GameGenerationController.class);
+
+    public List<Integer> getAllGenerationIds() {
+        return this.gameGenerationRepository.findAll().stream().map(GameGeneration::getGenerationId).collect(Collectors.toList());
+    }
+}
