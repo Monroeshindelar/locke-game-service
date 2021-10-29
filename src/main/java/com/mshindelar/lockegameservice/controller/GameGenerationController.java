4e6baa3337a0fbfacfee92ded8a3fcd9ff9d804b
Monroe Shindelar <monroeshindelar@gmail.com>
2021-10-28 21:59:35 -0700

    LW-28-Encounter-Generation
    Adding endpoint to get game generation information

--- src/main/java/com/mshindelar/lockegameservice/controller/GameGenerationController.java
+++ src/main/java/com/mshindelar/lockegameservice/controller/GameGenerationController.java
@@ -1,11 +1,13 @@
 package com.mshindelar.lockegameservice.controller;
 
+import com.mshindelar.lockegameservice.entity.generic.GameGeneration;
 import com.mshindelar.lockegameservice.service.GameGenerationService;
 import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -25,4 +27,7 @@ public class GameGenerationController {
     private List<Integer> getAllGenerationIds() {
         return this.gameGenerationService.getAllGenerationIds();
     }
+
+    @GetMapping("{generationId}")
+    private GameGeneration getByGenerationId(@PathVariable("generationId") int generationId) { return this.gameGenerationService.getByGenerationId(generationId); }
 }
